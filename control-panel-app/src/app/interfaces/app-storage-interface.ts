578fa06ff37eac12515ec2d95e695d049e10394e
ptruskovsky <ptruskovsky@gmail.com>
2019-04-06 22:08:39 +0300

    AppStorageInterface update

--- control-panel-app/src/app/interfaces/app-storage-interface.ts
+++ control-panel-app/src/app/interfaces/app-storage-interface.ts
@@ -1,7 +1,7 @@
 import {AppStorageKey} from '../models/app-storage-key';
 
 export interface AppStorageInterface {
-    setValue<T>(value: T, prefix: string | null);
+    setValue<T>(key: AppStorageKey, value: T, prefix: string | null);
     getValue<T>(key: AppStorageKey, prefix: string | null): T;
     removeValue(key: AppStorageKey, prefix: string | null);
 }
