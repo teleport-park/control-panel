978c5b83941fa1cc7e2b2c212301bf57ce6869e0
Nikolay Kovaliou <sonny30091984@yandex.ru>
2019-04-27 22:04:18 +0300

    remove unused code, add search icon

--- control-panel-app/src/app/admin-module/admin-modules/billing/income-tariffs/income-tariffs.component.ts
+++ control-panel-app/src/app/admin-module/admin-modules/billing/income-tariffs/income-tariffs.component.ts
@@ -5,29 +5,6 @@ import { filter, takeUntil } from 'rxjs/operators';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Package } from '../../../../common/shared-module/control-panel-ui-package/control-panel-ui-package.model';
 
-/**
- * tree node interface
- */
-export interface TreeNode {
-  children?: TreeNode[];
-  data?: NodeData;
-}
-
-interface NodeData {
-  name: string;
-  amount: number;
-  currency: 'BYN' | 'COIN' | '';
-  isRoot?: boolean;
-  isLast?: boolean;
-  isParentLast?: boolean;
-}
-
-interface FlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-  data: NodeData;
-}
 
 @Component({
   selector: 'income-tariffs',
@@ -36,187 +13,21 @@ interface FlatNode {
 })
 export class IncomeTariffsComponent implements OnInit {
 
-  destroyed$: Subject<boolean> = new Subject();
-  //
-  // /**
-  //  * tree control
-  //  */
-  // treeControl: FlatTreeControl<FlatNode>;
-  //
-  // /**
-  //  * tree flattener
-  //  */
-  // treeFlattener: MatTreeFlattener<TreeNode, FlatNode>;
-  //
-  // /**
-  //  * data source
-  //  */
-  // dataSource: MatTreeFlatDataSource<TreeNode, FlatNode>;
-
-  data: Package[] = [
-    {
-      packageInfo: {
-        id: 1,
-        name: 'TestPackage#1',
-        amount: 100,
-        currency: 'BYN'
-      },
-      payments: [{
-        index: 1,
-        id: 1,
-        amount: 10,
-        currency: 'COIN',
-        children: [{
-          name: 'test#1',
-          amount: 5,
-          currency: 'COIN'
-        }, {
-          name: 'test#2',
-          amount: 5,
-          currency: 'COIN'
-        }, {
-          name: 'test#3',
-          amount: 5,
-          currency: 'COIN'
-        }]
-      }, {
-        index: 2,
-        id: 2,
-        amount: 10,
-        currency: 'COIN'
-      }, {
-        index: 3,
-        id: 3,
-        amount: 10,
-        currency: 'COIN'
-      }]
-    }, {
-      packageInfo: {
-        id: 1,
-        name: 'TestPackage#2',
-        amount: 100,
-        currency: 'BYN'
-      },
-      payments: [{
-        index: 1,
-        id: 1,
-        amount: 10,
-        currency: 'COIN'
-      }, {
-        index: 2,
-        id: 2,
-        amount: 10,
-        currency: 'COIN'
-      }, {
-        index: 3,
-        id: 3,
-        amount: 10,
-        currency: 'COIN'
-      }]
-    }, {
-      packageInfo: {
-        id: 1,
-        name: 'TestPackage#3',
-        amount: 100,
-        currency: 'BYN'
-      },
-      payments: [{
-        index: 1,
-        id: 1,
-        amount: 10,
-        currency: 'COIN'
-      }, {
-        index: 2,
-        id: 2,
-        amount: 10,
-        currency: 'COIN'
-      }, {
-        index: 3,
-        id: 3,
-        amount: 10,
-        currency: 'COIN'
-      }]
-    }
-  ];
+  data: Package[] = [];
+  private destroyed$: Subject<boolean> = new Subject();
 
   constructor(public service: IncomeTariffsService, private cd: ChangeDetectorRef) {
   }
-
-  /**
-   * has child
-   * @param _
-   * @param node
-   */
-  hasChild = (_: number, node: FlatNode) => node.expandable;
 
   ngOnInit() {
     this.service.tariffs$.pipe(takeUntil(this.destroyed$), filter(data => !!data))
       .subscribe((result: Package[]) => {
         this.data = result;
-        // this.initDataSource(result);
-        // const rootNodes = this.dataSource._flattenedData.getValue();
-        // this.treeControl.expand(rootNodes[0]);
         this.cd.markForCheck();
         this.cd.detectChanges();
       });
   }
 
-  // /**
-  //  * init data
-  //  * @param data
-  //  */
-  // private initDataSource(data: TreeNode[]) {
-  //   this.setLastNode(data[0]);
-  //   this.treeControl = new FlatTreeControl<FlatNode>(
-  //     node => node.level, node => node.expandable);
-  //   this.treeFlattener = new MatTreeFlattener(
-  //     this.transformer, node => node.level, node => node.expandable, node => node.children);
-  //   this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-  //   this.dataSource.data = data;
-  // }
-
-  // /**
-  //  * set last nodes
-  //  * @param data
-  //  */
-  // private setLastNode(data: TreeNode) {
-  //   if (data.children && data.children.length) {
-  //     const lastNode = data.children[data.children.length - 1];
-  //     lastNode.data.isLast = true;
-  //     lastNode.data.isParentLast = true;
-  //     lastNode.children.forEach(child => {
-  //       child.data.isParentLast = true;
-  //     });
-  //     data.children.forEach(item => {
-  //       if (item.children && item.children.length) {
-  //         item.children[item.children.length - 1].data.isLast = true;
-  //       }
-  //     });
-  //   }
-  // }
-
-  // /**
-  //  * get node name
-  //  * @param node
-  //  */
-  // getNodeName(node: TreeNode): string {
-  //   return `${node.data.name} ${node.data.amount ? node.data.amount : ''} ${node.data.currency ? node.data.currency : ''}`;
-  // }
-  //
-  // /**
-  //  * transformer
-  //  * @param node
-  //  * @param level
-  //  */
-  // private transformer = (node: TreeNode, level: number) => {
-  //   return {
-  //     expandable: !!node.children && node.children.length > 0,
-  //     name: node.data.name,
-  //     level,
-  //     data: node.data
-  //   };
-  // }
-  //
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.data, event.previousIndex, event.currentIndex);
   }
