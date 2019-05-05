a68f5438fb219cacada1c7df172a82741ff3e29e
Nikolay Kovaliou <sonny30091984@yandex.ru>
2019-05-05 13:12:48 +0300

    add "Accept-Language" header

--- control-panel-app/src/app/common/auth-module/interceptors/auth.interceptor.ts
+++ control-panel-app/src/app/common/auth-module/interceptors/auth.interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
+import { TranslateService } from '../../translations-module';
 
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private loginService: AuthService) {
+  constructor(private loginService: AuthService, private translateService: TranslateService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -14,7 +15,8 @@ export class AuthInterceptor implements HttpInterceptor {
     if (user && user.token) {
       req = req.clone({
         setHeaders: {
-          Authorization: `Bearer ${user.token}`
+          Authorization: `Bearer ${user.token}`,
+          'Accept-Language': this.translateService.locale.getValue()
         }
       });
     }
