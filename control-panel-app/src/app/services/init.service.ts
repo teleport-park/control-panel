f739f83c86e6e8671ea796c8ccd34dfd0d212be4
Nikolay Kovaliou <sonny30091984@yandex.ru>
2019-06-11 22:49:22 +0300

    remove "app-init-config.json" from bundle, add default value

--- control-panel-app/src/app/services/init.service.ts
+++ control-panel-app/src/app/services/init.service.ts
@@ -4,39 +4,42 @@ import { Injectable } from '@angular/core';
  * config interface
  */
 export interface InitConfig {
-  api_url: string;
+    api_url: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 
 export class InitService {
 
-  /**
-   * init config
-   */
-  config: InitConfig;
+    /**
+     * init config
+     */
+    config: InitConfig = {api_url: '/'};
 
-  constructor() {  }
+    constructor() {
+    }
 
-  /**
-   * init app
-   */
-  init() {
-    return new Promise<void>((resolve, reject) => {
-      try {
-        this.config = require('../../config/app-init-config.json');
-        if (this.config && this.config.api_url) {
-          console.log('[API_URL]:', this.config.api_url);
-          resolve();
-          return;
-        } else {
-          reject('Property "api_url" is not defined');
-        }
-      } catch (e) {
-        reject('File app-init-config.json is missing');
-      }
-    });
-  }
+    /**
+     * init app
+     */
+    init() {
+        return new Promise<void>((resolve, reject) => {
+            try {
+                const config = require('../../config/app-init-config.json');
+                if (config && config.api_url) {
+                    console.log('[API_URL]:', config.api_url);
+                    this.config = config;
+                    resolve();
+                } else {
+                    console.warn('Property "api_url" is not defined in "app-init-config.json", default value "/" has been used')
+                    resolve();
+                }
+            } catch (e) {
+                console.warn('File app-init-config.json is missing, default value "/" has been used');
+                resolve();
+            }
+        });
+    }
 }
