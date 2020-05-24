130413bf784fb2e6e01ca5db2e73cad502aab89e
Nikolay Kovaliou <sonny30091984@yandex.ru>
2020-05-24 12:37:37 +0300

    transaction time filter implementation

--- control-panel-app/src/app/admin-module/admin-modules/park/billing/components/transactions/transactions.component.ts
+++ control-panel-app/src/app/admin-module/admin-modules/park/billing/components/transactions/transactions.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Component, Inject, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { TransactionsService } from './transactions.service';
 import { PaginationSetting } from '../../../../../../models/helpers/request-helper.interface';
 import { TranslateService } from '../../../../../../common/translations-module';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { ExtendedFilterUrlParamsInterface } from '../../../../../../interfaces/extended-filter-url-params.interface';
 
 @Component({
     selector: 'transactions',
@@ -18,11 +19,27 @@ export class TransactionsComponent implements OnInit {
 
     displayedColumns: string[] = ['id', 'account_id', 'created_at', 'amount'];
 
-    constructor(public service: TransactionsService, public translations: TranslateService) {
+    constructor(public service: TransactionsService, public translations: TranslateService,
+                @Inject('ExtendedFilterUrlParamsInterface') private extendedFilterUrlBuilder: ExtendedFilterUrlParamsInterface) {
     }
 
     ngOnInit() {
         this.service.getTransactions();
+    }
+
+    applyFilter(event: Partial<{time: {t: string, f: string} | null}>) {
+        if (event.time) {
+            this.service.requestHelper.setExtendedFilterRequest(this.extendedFilterUrlBuilder.getExtendedFilterParams(event.time));
+            this.service.requestHelper.filterData = event.time;
+            this.service.requestHelper.resetPagination();
+            this.resetPagination();
+            this.service.getTransactions();
+        } else {
+            this.service.requestHelper.resetFilterRequest();
+            this.service.requestHelper.resetPagination();
+            this.resetPagination();
+            this.service.getTransactions();
+        }
     }
 
     /**
@@ -36,6 +53,12 @@ export class TransactionsComponent implements OnInit {
         });
         this.service.requestHelper.setPagination({limit: event.pageSize, offset: event.pageSize * event.pageIndex} as PaginationSetting);
         this.service.getTransactions();
+    }
+
+    resetPagination() {
+        this.paginator && this.paginator.toArray().forEach(p => {
+            p.pageIndex = 0;
+        });
     }
 
 }
--- control-panel-app/src/app/common/shared-module/control-panel-ui-period-selector/control-panel-ui-period-selector.component.ts
+++ control-panel-app/src/app/common/shared-module/control-panel-ui-period-selector/control-panel-ui-period-selector.component.ts
@@ -1,124 +1,146 @@
-import { Component, ElementRef, EventEmitter, Input, Output, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, QueryList, Renderer2, ViewChildren } from '@angular/core';
 import { Moment } from 'moment';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 const moment = require('moment');
 
 export interface PeriodSelectorSubmitEvent {
-   from: string;
-   to: string;
-   fromMoment: Moment;
-   toMoment: Moment;
+    from: string;
+    to: string;
+    fromMoment: Moment;
+    toMoment: Moment;
 }
 
 @Component({
-   selector: 'control-panel-ui-period-selector',
-   templateUrl: './control-panel-ui-period-selector.component.html',
-   styleUrls: ['./control-panel-ui-period-selector.component.scss']
+    selector: 'control-panel-ui-period-selector',
+    templateUrl: './control-panel-ui-period-selector.component.html',
+    styleUrls: ['./control-panel-ui-period-selector.component.scss']
 })
 export class ControlPanelUiPeriodSelectorComponent {
 
-   @ViewChildren('fast_link') linkList: QueryList<ElementRef>;
+    @ViewChildren('fast_link') linkList: QueryList<ElementRef>;
 
-   timeStamp = moment();
+    timeStamp = moment();
 
-   /**
-    * from input label
-    */
-   @Input() fromLabel = 'From';
+    /**
+     * from input label
+     */
+    @Input() fromLabel = 'From';
 
-   /**
-    * to input label
-    */
-   @Input() toLabel = 'To';
+    /**
+     * to input label
+     */
+    @Input() toLabel = 'To';
 
-   /**
-    * select period button label
-    */
-   @Input() submitLabel = 'Select period';
+    /**
+     * select period button label
+     */
+    @Input() submitLabel = 'Select period';
 
-   /**
-    * format for date, see https://momentjs.com/docs/#/displaying/format/
-    */
-   @Input() format = 'DD.MM.YYYY';
+    /**
+     * format for date, see https://momentjs.com/docs/#/displaying/format/
+     */
+    @Input() format = 'DD.MM.YYYY';
 
-   /**
-    * max date value
-    */
-   @Input() maxDate: Moment = this.timeStamp;
+    /**
+     * max date value
+     */
+    @Input() maxDate: Moment = this.timeStamp;
 
-   @Input() minDate: Moment;
+    @Input() minDate: Moment;
 
-   /**
-    * output event
-    * @return PeriodSelectorSubmitEvent
-    */
-   @Output() selectPeriod: EventEmitter<PeriodSelectorSubmitEvent> = new EventEmitter();
+    /**
+     * output event
+     * @return PeriodSelectorSubmitEvent
+     */
+    @Output() selectPeriod: EventEmitter<PeriodSelectorSubmitEvent> = new EventEmitter();
 
-   /**
-    * from date form control
-    */
-   _fromDateControl = new FormControl(null);
-   /**
-    * from date form control
-    */
-   _fromTimeControl = new FormControl(null);
+    /**
+     * from date form control
+     */
+    _fromDateControl = new FormControl(null);
+    /**
+     * from date form control
+     */
+    _fromTimeControl = new FormControl(null);
 
-   /**
-    * to date form control
-    */
-   _toDateControl = new FormControl(this.maxDate);
-   /**
-    * to date form control
-    */
-   _toTimeControl = new FormControl(this.timeStamp.format('HH:mm'));
+    /**
+     * to date form control
+     */
+    _toDateControl = new FormControl(this.maxDate);
+    /**
+     * to date form control
+     */
+    _toTimeControl = new FormControl(this.timeStamp.format('HH:mm'));
 
 
-   constructor(private renderer: Renderer2) {
-   }
+    constructor(private renderer: Renderer2) {
+    }
 
-   public setPeriod(unit: string, value: number, index: number) {
-      console.log(this.linkList);
-      this.linkList.forEach(link => {
-         this.renderer.removeClass(link.nativeElement, 'active');
-      });
-      this.renderer.addClass(this.linkList.toArray()[index].nativeElement, 'active');
-      this.timeStamp = moment();
-      const fromDate = moment().subtract(value, unit);
-      this._fromTimeControl.setValue(fromDate.format('HH:mm'));
-      this._fromDateControl.setValue(fromDate);
-   }
+    public setPeriod(unit: string, value: number, index: number) {
+        this.linkList.forEach(link => {
+            this.renderer.removeClass(link.nativeElement, 'active');
+        });
+        this.renderer.addClass(this.linkList.toArray()[index].nativeElement, 'active');
+        this.timeStamp = moment();
+        const fromDate = moment().subtract(value, unit);
+        this._fromTimeControl.setValue(fromDate.format('HH:mm'));
+        this._fromDateControl.setValue(fromDate);
+    }
 
-   /**
-    * submit handler
-    */
-   submit() {
-      if (this.validateControl(this._fromDateControl) && this.validateControl(this._toDateControl)) {
-         this.selectPeriod.emit(this.preparePeriod());
-         return;
-      }
-      this._fromDateControl.markAsTouched();
-      this._toDateControl.markAsTouched();
-   }
+    /**
+     * submit handler
+     */
+    submit() {
+        if (this.validateControl(this._fromDateControl) && this.validateControl(this._toDateControl)) {
+            this.selectPeriod.emit(this.preparePeriod());
+            return;
+        }
+        this._fromDateControl.markAsTouched();
+        this._toDateControl.markAsTouched();
+    }
 
-   /**
-    * validate control
-    * @param control
-    */
-   private validateControl(control: FormControl): boolean {
-      return !!control.value && !control.errors;
-   }
+    /**
+     * validate control
+     * @param control
+     */
+    private validateControl(control: FormControl): boolean {
+        return !!control.value && !control.errors;
+    }
 
-   /**
-    * prepare event<PeriodSelectorSubmitEvent>
-    */
-   private preparePeriod(): PeriodSelectorSubmitEvent {
-      return {
-         from: moment(this._fromDateControl.value).format(this.format),
-         to: moment(this._toDateControl).format(this.format),
-         fromMoment: this._fromDateControl.value,
-         toMoment: this._toDateControl.value
-      };
-   }
+    /**
+     * prepare event<PeriodSelectorSubmitEvent>
+     */
+    private preparePeriod(): PeriodSelectorSubmitEvent {
+        const from = this._fromDateControl.value as Moment;
+        const to = this._toDateControl.value as Moment;
+        from.set({
+            hour: this._fromTimeControl.value.split(':')[0],
+            minute: this._fromTimeControl.value.split(':')[1]
+        });
+        to.set({
+            hour: this._toTimeControl.value.split(':')[0],
+            minute: this._toTimeControl.value.split(':')[1]
+        });
+
+        return {
+            from: moment(this._fromDateControl.value).format(this.format),
+            to: moment(this._toDateControl).format(this.format),
+            fromMoment: from,
+            toMoment: to
+        };
+    }
+
+    reset() {
+        this.linkList.forEach(link => {
+            this.renderer.removeClass(link.nativeElement, 'active');
+        });
+        this._fromTimeControl.setValue(null);
+        this._fromDateControl.setValue(null);
+        this.timeStamp = moment();
+        this._toTimeControl.setValue(this.timeStamp.format('HH:mm'));
+        this._toDateControl.setValue(this.timeStamp);
+        this.selectPeriod.emit(null);
+    }
 
 }
